af97719490a2d1b9f7f037a1a13d36612301985c
adnankhanmru <adnankgh052@gmail.com>
2024-07-18 13:30:42 +0530

    wallet removed

--- wallet-app-backend/walletapp/src/main/java/com/example/walletapp/walletapp/config/SecurityConfig.java
+++ wallet-app-backend/walletapp/src/main/java/com/example/walletapp/walletapp/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.walletapp.walletapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -46,7 +45,6 @@ public class SecurityConfig {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withUsername("user")
                 .password(passwordEncoder().encode("password"))
-                .roles("USER")
                 .build());
         return manager;
     }
--- wallet-app-backend/walletapp/src/main/java/com/example/walletapp/walletapp/controller/AuthController.java
+++ wallet-app-backend/walletapp/src/main/java/com/example/walletapp/walletapp/controller/AuthController.java
@@ -1,11 +1,14 @@
 package com.example.walletapp.walletapp.controller;
 
+import com.example.walletapp.walletapp.dto.LoginRequest;
 import com.example.walletapp.walletapp.model.User;
 import com.example.walletapp.walletapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/auth")
@@ -16,6 +19,17 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
+        User currentUser = userRepository.findByUsername(loginRequest.getUsername());
+
+        if (currentUser == null || !passwordEncoder.matches(loginRequest.getPassword(), currentUser.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(currentUser);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user) {
